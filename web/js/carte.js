a9953e43cb60507203c8d24e25b8d58d9bea90b6
Alexandre <a.noel054@gmail.com>
2023-06-14 17:56:08 +0200

    refator: responsive et liens

--- web/js/carte.js
+++ web/js/carte.js
@@ -3,3 +3,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var marker = L.marker([48.6864306, 6.1697993]).addTo(map);
+marker.bindPopup("<b>Les P'tits Oignons</b><br>48 Rue Jeanne d'Arc");
