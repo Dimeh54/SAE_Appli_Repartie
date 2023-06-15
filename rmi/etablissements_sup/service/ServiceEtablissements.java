4f718f0788b382745cb5eecfc6b0faed6f1ee132
Mehdi <mehdi.allali1@etu.univ-lorraine.fr>
2023-06-15 17:51:58 +0200

    ajout d'un proxy dans la requête http data.gouv.fr

--- rmi/etablissements_sup/service/ServiceEtablissements.java
+++ rmi/etablissements_sup/service/ServiceEtablissements.java
@@ -1,5 +1,7 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.ProxySelector;
+import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -15,15 +17,23 @@ public class ServiceEtablissements implements InterfaceEtablissements {
 
     public static void main(String[] args) throws RemoteException, FileNotFoundException, ServerNotActiveException {
         //new ServiceEtablissements().recupererEtablissements();
-        String url = "https://www.data.gouv.fr/fr/datasets/r/5fb6d2e3-609c-481d-9104-350e9ca134fa";
+        //String url = "https://www.data.gouv.fr/fr/datasets/r/5fb6d2e3-609c-481d-9104-350e9ca134fa";
+        String url = "https://data.enseignementsup-recherche.gouv.fr//explore/dataset/fr-esr-principaux-etablissements-enseignement-superieur/download?format=json&amp;timezone=Europe/Berlin&amp;use_labels_for_header=false";
 
-        HttpClient httpClient = HttpClient.newHttpClient();
+        String urlProxy = "www-cache";
+        int port = 3128;
+        //HttpClient httpClient = HttpClient.newHttpClient();
+        HttpClient httpClient = HttpClient.newBuilder().proxy(ProxySelector.of(new InetSocketAddress(urlProxy, port))).build();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .build();
 
         try {
+            System.out.println("avant");
+            
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("apres");
 
             int statusCode = response.statusCode();
             System.out.println("Status Code: " + statusCode);
