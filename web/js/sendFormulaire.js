8f52949fbc13186c8a6b286d502f8e83cc51b30e
Alex Collin <alexcollin03@orange.fr>
2023-06-19 11:19:59 +0200

    légère modification dans send formulaire pour que l'on puisse traiter la requete

--- web/js/sendFormulaire.js
+++ web/js/sendFormulaire.js
@@ -45,12 +45,11 @@ async function envoyerFormulaire(idRestaurant) {
     const informations = {
         nom: formData.get('nom'),
         prenom: formData.get('prenom'),
-        telephone: formData.get('phone'),
-        personnes: formData.get('personnes'),
-        date: formData.get('date'),
-        heure: formData.get('heure'),
+        numtel: formData.get('phone'),
+        nbpers: formData.get('personnes'),
+        date : (formData.get('date') + " " + formData.get('heure')),
         restaurant: formData.get('restaurant'),
-        id: idRestaurant
+        id_restaurant: idRestaurant
     };
 
     // Envoyer les informations au serveur
