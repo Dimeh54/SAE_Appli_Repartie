a5d05a6386d428368cc0f486a5b34d5359a62e23
Mehdi <mehdi.allali1@etu.univ-lorraine.fr>
2023-06-21 17:48:45 +0200

    retour du message d'erreur

--- web/js/sendFormulaire.js
+++ web/js/sendFormulaire.js
@@ -80,7 +80,7 @@ async function envoyerFormulaire(idRestaurant = null, adresse = "http://localhos
     // Afficher un message de confirmation
     loadingIcon.style.display = 'none';
     let message = document.querySelector('#response');
-    if (response.ok) {
+    if (response) {
         message.innerHTML = "Votre réservation a bien été prise en compte !";
         message.style.display = 'block';
     }
