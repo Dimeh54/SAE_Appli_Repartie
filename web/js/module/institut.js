04cb058cf9844fdd5cb5ce36db0748e29f953d6e
Alexandre <a.noel054@gmail.com>
2023-06-21 16:28:22 +0200

    fix

--- web/js/module/institut.js
+++ web/js/module/institut.js
@@ -38,7 +38,7 @@ function displayOnMap(map, tab_institut) {
  * @return tableau d'objets avec les infos des instituts supérieurs
  */
 async function creerTabInstitut(adresse) {
-    let url_institut = adresse + "/api/etalissements";
+    let url_institut = adresse + "/api/etablissements";
     let urlSecours = "../Ressources/etablissements.json";
 
     // On fait un appel aux infos des instituts supérieurs
